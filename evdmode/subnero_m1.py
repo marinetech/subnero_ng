58f2980386a965331e3be3be3555e7ce9fec59be
Ilan Mardix <ilan.mardix@gmail.com>
2020-01-30 12:04:49 +0200

    added ev mode

---
+++ evdmode/subnero_m1.py
@@ -0,0 +1,57 @@
+modem = '192.168.0.11'
+number_of_recs = 3
+rec_size = 12000
+rx_out_folder = "rx_out"
+rx_out_file = "rx_rec_"
+sleep_between_loops = 2 #seconds
+
+#----------------------------------------#
+import sys
+import os
+import time
+import numpy as np
+import arlpy as ap
+from unetpy import *
+
+
+# Open connection to the modem
+print("-I- connecting to modem: " + modem)
+try:
+    sock = UnetSocket(modem, 1100)
+    gw = sock.getGateway()
+except:
+    exit(1)
+
+# If necessary, create out folder
+i = 1
+while os.path.exists(rx_out_folder):
+        rx_out_folder = rx_out_folder + "_" + str(i)
+        i = i + 1
+os.makedirs(rx_out_folder)
+
+
+# lookup the agent providing baseband service
+bb = gw.agentForService(Services.BASEBAND)
+
+file_idx = 0
+for i in range(0,number_of_recs):
+    print("-I- rx #" + str(i+1))
+    # Record a baseband signal
+    bb << RecordBasebandSignalReq(recLen=rec_size)
+
+    rxntf = gw.receive(RxBasebandSignalNtf, timeout=5000)
+    if rxntf is not None:
+        # what is the next avaiable file name?
+        next_out_file = "{}/{}{}".format(rx_out_folder, rx_out_file, file_idx)
+        file_idx = file_idx + 1
+
+        # Extract the recorded signal
+        rec_signal = rxntf.signal
+        # rec_signal.tofile(next_out_file)
+        np.savetxt(next_out_file, rec_signal)
+        print('-I- signal was recorded successfully')
+    else:
+        print('-E- signal was not recorded successfully')
+
+
+sock.close()
---
+++ evdmode/subnero_m2.py
@@ -0,0 +1,42 @@
+modem = '192.168.0.11'
+power_level = -60 #Max:0; Min:-138
+signal_file = '../signals/subnero.sig'
+number_of_loops = 3
+sleep_between_loops = 2 #seconds
+
+#---------------------------------------#
+from unetpy import *
+from arlpy import *
+import numpy as np
+import time
+
+print("-I- connecting to modem: " + modem)
+try:
+    sock = UnetSocket(modem, 1100)
+    gw = sock.getGateway()
+except:
+    print("-E- Failed")
+    exit()
+
+print("-I- loading signal from: " + signal_file)
+try:
+    signal = np.loadtxt(signal_file)
+except Exception as ex:
+    print(str(ex))
+    print("-E- failed to load signal file")
+    exit(1)
+
+# lookup the agent providing baseband service
+bb = gw.agentForService(Services.BASEBAND)
+for i in range(0, number_of_loops):
+    print("-I- Tx #" + str(i+1) + " " + signal_file)
+    bb << TxBasebandSignalReq(signal=signal.tolist(), fc=0, fs=192000)
+    txntf = gw.receive(TxFrameNtf, 5000)
+    if txntf is None:
+        print('-E- transmission not successfull')
+    if sleep_between_loops:
+        print("-I- sleeping for {} seconds".format(sleep_between_loops))
+        time.sleep(sleep_between_loops)
+
+
+sock.close()
